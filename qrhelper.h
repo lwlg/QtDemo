916787c44364cd738f9f1c2aa7e9fcf68f357c6c
lwlg <452406859@qq.com>
2019-12-06 15:07:51 +0800

    Add files via upload

---
+++ qrhelper.h
@@ -0,0 +1,30 @@
+﻿/*
+    https://fukuchi.org/works/qrencode/manual/index.html
+*/
+
+#ifndef QRHELPER_H
+#define QRHELPER_H
+
+#include <QObject>
+#include <QPixmap>
+
+
+class QRHelper : public QObject
+{
+    Q_OBJECT
+public:
+    explicit QRHelper(QObject *parent = 0);
+
+    QPixmap generateQR(QString strContent, int width = 300, int height = 300);
+signals:
+
+public slots:
+
+
+
+private:
+
+
+};
+
+#endif // QRHELPER_H
